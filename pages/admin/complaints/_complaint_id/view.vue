52c687c4c25d00e41e078b32bdd0f4ff7a5910a7
Catalin Iacob <catalin.iacob@web-group.ro>
2023-07-12 20:36:40 +0300

    update view complaint

--- pages/admin/complaints/_complaint_id/view.vue
+++ pages/admin/complaints/_complaint_id/view.vue
@@ -1,108 +1,170 @@
 <template>
-  <div class="complaint-view-card">
-    <v-row>
-      <v-col cols="12" md="10">
-        <ui-form-card v-if="!loading">
-          <template #title>
-            <span v-if="complaint?.type === 'hurt'">Raportare abuz</span>
-            <span v-if="complaint?.type === 'move'">Cerere relocare</span>
-            <span v-if="complaint?.type === 'evaluation'">Cerere reexaminare</span>
-          </template>
-          <template #subtitle>
-            {{ complaint.created_at }}
-          </template>
-          <template>
+
+  <dashboard-page>
+    <template #navigation>
+      <ui-form-back-btn to="/admin/complaints"/>
+    </template>
+    <template #title>Vizualizare Raportare</template>
+    <template #workspace>
+      <v-row>
+        <v-col>
+          <div class="complaint-view-card">
             <v-row>
-              <v-col cols="12" md="6">
-                <label class="complaint-label">Pentru cine a fost raportat</label>
-                <div v-if="complaint.victim === 'me'">Pentru propria persoana</div>
-                <div v-if="complaint.victim === 'other'">Pentru altcineva</div>
-              </v-col>
-              <v-col cols="12" md="6">
-                <label class="complaint-label">Numar de inregistrare plangere</label>
-                <v-text-field
-                    outlined
-                    dense
-                    v-model="complaint.register_number"
-                    :disabled="!editMode"
-                    :loading="loadingSaveRegisterNumber"
-                >
-                  <template #append>
-                      <v-icon v-if="!editMode" @click="editMode = true">mdi-pencil</v-icon>
-                      <v-icon v-if="editMode" @click="saveRegisterNumber">mdi-check</v-icon>
+              <v-col cols="12" md="10">
+                <ui-form-card v-if="!loading">
+                  <template #title>
+                    <span v-if="complaint?.type === 'hurt'">Raportare abuz</span>
+                    <span v-if="complaint?.type === 'move'">Cerere relocare</span>
+                    <span v-if="complaint?.type === 'evaluation'">Cerere reexaminare</span>
                   </template>
-                </v-text-field>
-              </v-col>
-              <v-col cols="12" md="6">
-                <label class="complaint-label">Nume si prenume</label>
-                <div>{{ complaint.name }}</div>
-              </v-col>
-              <v-col cols="12" md="6">
-                <label class="complaint-label">Oras</label>
-                <div>{{  complaint.city_name }} {{ complaint.county_name }}</div>
-              </v-col>
-              <v-col cols="12" md="6">
-                <label class="complaint-label">Centru</label>
-                <div>{{ complaint.location_name }}</div>
-              </v-col>
-              <v-col cols="12" md="6">
-                <label class="complaint-label">Tip centru</label>
-                <div>{{ complaint?.location?.type }}</div>
-              </v-col>
-              <v-col cols="12">
-                <label class="complaint-label">Email transmis</label>
-                <p>
-                  Subsemnatul/a Emilia Eminovici, mă aflu în orașul Bacău, în Centrul de zi “Ioana Stoenescu”, de tipul Centru de zi și declar că:
-
-                  Am fost bătut/ă;
-                  Nu am primit mâncare;
-                  Am fost închis/ă în cameră.
-
-                  Vă rog să luați măsurile necesare. Va solicit confirmarea primirii si numarul de inregistrare al acestei cereri.
-                </p>
-              </v-col>
-              <v-col cols="12">
-                <label class="complaint-label">Atasamente</label>
-                <div v-if="complaint.proof_type === 'later' ">Am dovezi si le pot oferi la cerere</div>
-                <div v-if="complaint.proof_type === 'no'">Nu am dovezi</div>
-                <v-list v-if="complaint.proof_type === 'yes'" one-line class="attachment-list">
-                  <template v-for="(item, index) in complaint?.uploads">
-                    <v-list-item>
-                      <v-list-item-content>
-                        <v-list-item-title class="text--primary">{{ item.name }}</v-list-item-title>
-                      </v-list-item-content>
-
-                      <v-list-item-action>
-
-                        <v-btn
-                          text
-                          plain
-                          color="primary"
-                          class="text-none"
-                          :href="item.dataUrl"
-                          target="_blank"
+                  <template #subtitle>
+                    {{ complaint.created_at }}
+                  </template>
+                  <template>
+                    <v-row>
+                      <v-col cols="12" md="6">
+                        <label class="complaint-label">Pentru cine a fost raportat</label>
+                        <div v-if="complaint.victim === 'me'">Pentru propria persoana</div>
+                        <div v-if="complaint.victim === 'other'">Pentru altcineva</div>
+                      </v-col>
+                      <v-col cols="12" md="6">
+                        <label class="complaint-label">Numar de inregistrare plangere</label>
+                        <v-text-field
+                            outlined
+                            dense
+                            v-model="complaint.register_number"
+                            :disabled="!editMode"
+                            :loading="loadingSaveRegisterNumber"
                         >
-                          Vizualizeaza
-                        </v-btn>
-                      </v-list-item-action>
-                    </v-list-item>
+                          <template #append>
+                            <v-icon v-if="!editMode" @click="editMode = true">mdi-pencil</v-icon>
+                            <v-icon v-if="editMode" @click="saveRegisterNumber">mdi-check</v-icon>
+                          </template>
+                        </v-text-field>
+                      </v-col>
+                      <v-col cols="12" md="6">
+                        <label class="complaint-label">Nume si prenume</label>
+                        <div>{{ complaint.name }}</div>
+                      </v-col>
+                      <v-col cols="12" md="6">
+                        <label class="complaint-label">Oras</label>
+                        <div>{{  complaint.city_name }} {{ complaint.county_name }}</div>
+                      </v-col>
+                      <v-col cols="12" md="6">
+                        <label class="complaint-label">Centru</label>
+                        <div>{{ complaint.location_name }}</div>
+                      </v-col>
+                      <v-col cols="12" md="6">
+                        <label class="complaint-label">Tip centru</label>
+                        <div>{{ complaint?.location?.type }}</div>
+                      </v-col>
+                      <v-col cols="12" md="6">
+                        <label class="complaint-label">Locatie pe harta</label>
+                        <div>
+                          <a v-if="complaint.lat" target="_blank" :href="`https://www.google.com/maps/place/${complaint.lat},${complaint.lng}`">Vezi locatia pe harta</a>
+                          <div v-else> Indisponibila </div>
+                        </div>
+                      </v-col>
+                      <v-col cols="12">
+                        <div style="border: 1px solid #dddddd; border-radius: 10px" class="pa-4">
+                          <label class="complaint-label">Email transmis</label>
 
+                          <div v-if="complaint.victim === 'me'" class="mb-4 mt-4">
+                            <div class="mb-4">
+                              Subsemnata/Subsemnatul {{ complaint.name }}
+                              <span v-if="complaint.county_name || complaint.location_name">, mă aflu în</span>
+                              <span v-if="complaint.county_name">{{ complaint.county_name }}</span>
+                              <span v-if="complaint.location_name">în {{ complaint.location_name }}</span>
+                              , și declar că:
+                            </div>
+
+                            <div v-if="complaint.type === 'hurt'" class="mb-4">
+                              <ul>
+                                <li v-for="d in complaint.detail_labels">{{ d }}</li>
+                              </ul>
+                            </div>
+
+                            <div v-if="complaint.type === 'move'" class="mb-4">
+                              <ul>
+                                <li>
+                                  Vreau sa fiu mutat/a la
+                                  <span v-if="complaint.location_to_name">{{ complaint.location_to_name }}</span>
+                                  <span v-else>alt centru</span>
+                                </li>
+                              </ul>
+                              <div class="mt-4" v-if="complaint.reason">Motivul pentru care vreau să mă mut este: <strong>{{ complaint.reason }}</strong></div>
+                            </div>
+
+                            <div v-if="complaint.type === 'evaluation'" class="mb-4">
+                              <ul>
+                                <li>Vreau să fiu evaluat/ă din nou la judecător</li>
+                              </ul>
+                            </div>
+                          </div>
+                          <div v-if="complaint.victim === 'other'" class="mb-4 mt-4">
+                            <div>
+                              Subsemnata/Subsemnatul {{ complaint.name }}, declar ca
+                              <span v-if="complaint.county_name">{{ complaint.county_name }} </span>
+                              <span v-if="complaint.location_name">în {{ complaint.location_name }}</span>
+                              s-au întâmplat următoarele
+                            </div>
+                            <div>
+                              <ul>
+                                <li>{{ complaint.reason }}</li>
+                              </ul>
+                            </div>
+                          </div>
+
+                          <div class="mb-4">
+                            Solicit ca datele mele personale să nu devină publice ca urmare a acestei plângeri, a cărei soluționare o cer.
+                          </div>
+
+                          <div v-if="complaint.proof_type === 'later' || complaint.proof_type === 'yes'" class="mb-4">
+                            <label class="complaint-label">Atasamente</label>
+                            <div v-if="complaint.proof_type === 'later'">Am dovezi si le pot oferi la cerere</div>
+                            <div v-if="complaint.proof_type === 'yes'">
+                              <div v-for="item in complaint.uploads">
+                                <a :href="item.dataUrl">{{item.name}}</a>
+                              </div>
+
+                            </div>
+                          </div>
+
+                          <div>
+                            <label class="complaint-label">Emailul a fost transmis la data {{ complaint.sent_at }} catre</label>
+                            <div>
+                              <div>
+                                <label>Denumire intitutii: </label>
+                                <ul>
+                                  <li v-for="(sti,index) in complaint.sent_to_institutions">
+                                    {{ sti }}
+                                  </li>
+                                </ul>
+                              </div>
+                              <div>
+                                <span>Adrese de email:</span>
+                                <ul>
+                                  <li v-for="(stm,index) in complaint.sent_to_emails">
+                                    {{ stm }}
+                                  </li>
+                                </ul>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </v-col>
+                    </v-row>
                   </template>
-                </v-list>
-              </v-col>
-              <v-col cols="12">
-                <label class="complaint-label">Emailul a fost transmis catre</label>
-                <div>
-                  <div>Secția de Poliție 7 Bacău politiebacau@gov.ro</div>
-                  <div>DGASPC Bacău dgaspc_bacau@gov.ro</div>
-                </div>
+                </ui-form-card>
               </v-col>
             </v-row>
-          </template>
-        </ui-form-card>
-      </v-col>
-    </v-row>
-  </div>
+          </div>
+        </v-col>
+      </v-row>
+    </template>
+
+  </dashboard-page>
+
 </template>
 
 <script>
